21546bc766d25552cbe436ef96d16470a1b9e317
Damam Rohini <damamrohini25@gmail.com>
2026-07-14 15:54:56 +0530

    Initial HouseRent project

--- server/routes/propertyRoutes.js
+++ server/routes/propertyRoutes.js
@@ -1,14 +1,9 @@
 const express = require("express");
-
 const router = express.Router();
-
 const {
   addProperty,
   getProperties,
 } = require("../controllers/propertyController");
-
 router.post("/", addProperty);
-
 router.get("/", getProperties);
-
 module.exports = router;
